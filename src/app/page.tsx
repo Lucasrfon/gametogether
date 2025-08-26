4d89b1f449cba3f9fb6efba1085ac4281b6b9bb3
Lucas Fonseca <lucasrfon@gmail.com>
2025-08-26 17:45:15 -0300

    fix: remove board from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import DotsAndBoxes from '@/components/DotsBoxes';
-
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -14,8 +12,6 @@ export default function Home() {
           <li className="mb-2 tracking-[-.01em]">Mill</li>
           <li className="mb-2 tracking-[-.01em]">Dots and Boxes</li>
         </ul>
-
-        <DotsAndBoxes />
       </main>
     </div>
   );
