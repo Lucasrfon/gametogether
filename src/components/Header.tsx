bae9c94484bacdede6b0ec4b48b7aa70e9030e90
Lucas Fonseca <lucasrfon@gmail.com>
2025-08-29 22:34:14 -0300

    style: header icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,10 @@
 'use client';
+import {
+  LucideHouse,
+  LucideNotebookText,
+  LucidePhone,
+  LucideSmartphoneCharging,
+} from 'lucide-react';
 import Link from 'next/link';
 
 export default function Header() {
@@ -10,17 +16,26 @@ export default function Header() {
     >
       <h1 className="text-xl font-bold">🚀 Play together, have fun together</h1>
       <nav className="flex gap-6">
-        <Link href="/" className="hover:text-blue-600">
-          Home
+        <Link href="/" className="hover:text-blue-600 flex items-center gap-1">
+          <LucideHouse className="h-4" /> Home
         </Link>
-        <Link href="/info" className="hover:text-blue-600">
-          About Project
+        <Link
+          href="/info"
+          className="hover:text-blue-600 flex items-center gap-1"
+        >
+          <LucideNotebookText className="h-4" /> About Project
         </Link>
-        <Link href="/support" className="hover:text-blue-600">
-          Insert Coin
+        <Link
+          href="/support"
+          className="hover:text-blue-600 flex items-center gap-1"
+        >
+          <LucideSmartphoneCharging className="h-4" /> Power up
         </Link>
-        <Link href="/contact" className="hover:text-blue-600">
-          Contact
+        <Link
+          href="/contact"
+          className="hover:text-blue-600 flex items-center gap-1"
+        >
+          <LucidePhone className="h-4" /> Contact
         </Link>
       </nav>
     </header>
