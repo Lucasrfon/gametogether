24dd5f98f39f745287d56099cb15dd7c2cb0a477
Lucas Fonseca <lucasrfon@gmail.com>
2025-08-31 17:04:08 -0300

    style: change 404 page text

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,10 +7,19 @@ export default function NotFound() {
     <div className="flex flex-col items-center justify-center min-h-screen gap-4 bg-gradient-to-br from-gray-950 via-gray-900 to-black">
       <Image src={NotFoundImage} alt="Not Found" width={400} height={300} />
       <h2>404</h2>
-      <p>Page under development</p>
+      <section className="space-y-6 text-lg text-gray-300 leading-relaxed">
+        <p>Page under development</p>
+        <p>Or will be soon.</p>
+        <p>
+          Or maybe you tried a path that doesn't exist. But don't worry — you
+          can go back home
+        </p>
+        <p>and find something fun to play!</p>
+        <p>No, this is not Dungeons & Dragons the TV series.</p>
+      </section>
       <Link
         href="/"
-        className="bg-amber-700 text-white px-4 py-2 rounded-lg hover:bg-amber-800 cursor-pointer"
+        className="bg-amber-700 text-white px-4 py-2 rounded-lg hover:bg-amber-800 cursor-pointer hover:scale-105 transition-all duration-200"
       >
         Return Home
       </Link>
